84def66a6fb4b5380d868ee4f0a84a9168fa36be
static <kmc7468@naver.com>
2021-05-23 20:09:17 +0900

    Interval 클래스 추가
    구간을 나타내는 클래스입니다.

--- include/StatSim/Distribution.hpp
+++ include/StatSim/Distribution.hpp
@@ -1,6 +1,33 @@
 #pragma once
 
+#include <numeric>
 #include <string>
+
+namespace StatSim {
+	class Interval final {
+	public:
+		static constexpr double Infinity = std::numeric_limits<double>::infinity();
+
+	private:
+		double m_Begin, m_End;
+		bool m_IsBeginOpen = false, m_IsEndOpen = false;
+
+	public:
+		Interval(double begin, double end) noexcept;
+		Interval(double begin, bool isBeginOpen, double end, bool isEndOpen) noexcept;
+		Interval(const Interval& interval) noexcept = default;
+		~Interval() = default;
+
+	public:
+		Interval& operator=(const Interval& interval) noexcept = default;
+
+	public:
+		double GetBegin() const noexcept;
+		double GetEnd() const noexcept;
+		bool IsBeginOpen() const noexcept;
+		bool IsEndOpen() const noexcept;
+	};
+}
 
 namespace StatSim {
 	class Distribution {
--- src/Distribution.cpp
+++ src/Distribution.cpp
@@ -1,6 +1,36 @@
 #include <StatSim/Distribution.hpp>
 
+#include <cassert>
 #include <cmath>
+
+namespace StatSim {
+	Interval::Interval(double begin, double end) noexcept
+		: Interval(begin, true, end, true) {}
+	Interval::Interval(double begin, bool isBeginOpen, double end, bool isEndOpen) noexcept
+		: m_Begin(begin), m_End(end), m_IsBeginOpen(isBeginOpen), m_IsEndOpen(isEndOpen) {
+		assert(begin < end);
+
+		if (begin == -Infinity) {
+			m_IsBeginOpen = true;
+		}
+		if (end == Infinity) {
+			m_IsEndOpen = true;
+		}
+	}
+
+	double Interval::GetBegin() const noexcept {
+		return m_Begin;
+	}
+	double Interval::GetEnd() const noexcept {
+		return m_End;
+	}
+	bool Interval::IsBeginOpen() const noexcept {
+		return m_IsBeginOpen;
+	}
+	bool Interval::IsEndOpen() const noexcept {
+		return m_IsEndOpen;
+	}
+}
 
 namespace StatSim {
 	double Distribution::GetVariance() const {
